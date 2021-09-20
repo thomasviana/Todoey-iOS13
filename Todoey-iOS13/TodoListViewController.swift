1804308795c10f330b305517b247aae843d8c925
Thomas Viana <thomasviana16@gmail.com>
2021-09-20 10:29:26 -0500

    Add table view and checkmark functionality

---
+++ Todoey-iOS13/TodoListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TodoListViewController.swift
+//  Todoey-iOS13
+//
+//  Created by Thomas Viana on 20/09/21.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = [
+        "Find Mike", "Buy Eggos", "Destroy Mdemogorgon"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
